91e9c7d3447acce83f106999ba5ba14260a162a8
timurIsakov <timur092000@gmail.com>
2023-12-29 00:36:10 +0500

    Fixed bags with scrolling page

--- lib/features/auth/presentation/screens/sign_in_part_one_screen.dart
+++ lib/features/auth/presentation/screens/sign_in_part_one_screen.dart
@@ -41,127 +41,139 @@ class _SignInPartOneScreenState extends State<SignInPartOneScreen> {
 
   @override
   Widget build(BuildContext context) {
-    //Email for continue
-    _textEditingController.text = "timur092000@gmail.com";
     return KeyboardDismisser(
       child: Scaffold(
-        body: Form(
-          key: _formKey,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20).r,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: SvgPicture.asset(
-                          Assets.tLeftArrowIcon,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      LinearPercentIndicator(
-                        barRadius: const Radius.circular(12).r,
-                        width: 120.w,
-                        lineHeight: 8.h,
-                        percent: 0.3,
-                        backgroundColor: const Color(0xff191B1C),
-                        progressColor: const Color(0xffEEEFF0),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(32).r,
-                            border: Border.all(color: const Color(0xffEEEFF0))),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                                  vertical: 8, horizontal: 16)
-                              .r,
-                          child: Text(
-                            "buttonHelp".tr(),
-                            style: TextStyle(
-                              fontFamily: "SfProDisplay",
-                              fontWeight: FontWeight.w500,
-                              fontSize: 12.sp,
-                              color: const Color(0xffEEEFF0),
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  child: IntrinsicHeight(
+                    child: SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20).r,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  icon: SvgPicture.asset(
+                                    Assets.tLeftArrowIcon,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                LinearPercentIndicator(
+                                  barRadius: const Radius.circular(12).r,
+                                  width: 120.w,
+                                  lineHeight: 8.h,
+                                  percent: 0.3,
+                                  backgroundColor: const Color(0xff191B1C),
+                                  progressColor: const Color(0xffEEEFF0),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(32).r,
+                                      border: Border.all(
+                                          color: const Color(0xffEEEFF0))),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                            vertical: 8, horizontal: 16)
+                                        .r,
+                                    child: Text(
+                                      "buttonHelp".tr(),
+                                      style: TextStyle(
+                                        fontFamily: "SfProDisplay",
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 12.sp,
+                                        color: const Color(0xffEEEFF0),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
                             ),
-                          ),
+                            SizedBox(
+                              height: 20.h,
+                            ),
+                            Text(
+                              "enterYourEmail".tr(),
+                              style: TextStyle(
+                                fontFamily: "SfProDisplay",
+                                fontWeight: FontWeight.w700,
+                                fontSize: 24.sp,
+                                color: const Color(0xffEEEFF0),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 8.h,
+                            ),
+                            Text(
+                              "signInPartOneDescriptionOne".tr(),
+                              style: TextStyle(
+                                fontFamily: "SfProDisplay",
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16.sp,
+                                color: const Color(0xffB9BFC1),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 24.h,
+                            ),
+                            SizedBox(
+                              child: InputWidget(
+                                text: "inputEmail".tr(),
+                                controller: _textEditingController,
+                                onChanged: (text) {
+                                  setState(() {});
+                                },
+                                validator: (value) {
+                                  return FormValidator.validateEmail(value);
+                                },
+                              ),
+                            ),
+                            const Spacer(),
+                            Text(
+                              "signInPartOneDescriptionTwo".tr(),
+                              style: TextStyle(
+                                fontFamily: "SfProDisplay",
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16.sp,
+                                color: const Color(0xffB9BFC1),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 24.h,
+                            ),
+                            ButtonWidget(
+                              text: "continue".tr(),
+                              onTap: () {
+                                AnimatedNavigation.push(
+                                    context: context,
+                                    page: const SignInPartTwoScreen());
+                              },
+                              isEnabled:
+                                  _formKey.currentState?.validate() ?? false,
+                            ),
+                            SizedBox(
+                              height: 45.h,
+                            ),
+                          ],
                         ),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  Text(
-                    "enterYourEmail".tr(),
-                    style: TextStyle(
-                      fontFamily: "SfProDisplay",
-                      fontWeight: FontWeight.w700,
-                      fontSize: 24.sp,
-                      color: const Color(0xffEEEFF0),
+                      ),
                     ),
                   ),
-                  SizedBox(
-                    height: 8.h,
-                  ),
-                  Text(
-                    "signInPartOneDescriptionOne".tr(),
-                    style: TextStyle(
-                      fontFamily: "SfProDisplay",
-                      fontWeight: FontWeight.w400,
-                      fontSize: 16.sp,
-                      color: const Color(0xffB9BFC1),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 24.h,
-                  ),
-                  SizedBox(
-                    child: InputWidget(
-                      text: "inputEmail".tr(),
-                      controller: _textEditingController,
-                      onChanged: (text) {
-                        setState(() {});
-                      },
-                      validator: (value) {
-                        return FormValidator.validateEmail(value);
-                      },
-                    ),
-                  ),
-                  const Spacer(),
-                  Text(
-                    "signInPartOneDescriptionTwo".tr(),
-                    style: TextStyle(
-                      fontFamily: "SfProDisplay",
-                      fontWeight: FontWeight.w400,
-                      fontSize: 16.sp,
-                      color: const Color(0xffB9BFC1),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 24.h,
-                  ),
-                  ButtonWidget(
-                    text: "continue".tr(),
-                    onTap: () {
-                      AnimatedNavigation.push(
-                          context: context, page: const SignInPartTwoScreen());
-                    },
-                    isEnabled: _formKey.currentState?.validate() ?? false,
-                  ),
-                  SizedBox(
-                    height: 45.h,
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
+            );
+          },
         ),
       ),
     );
--- lib/features/auth/presentation/screens/sign_in_part_three_screen.dart
+++ lib/features/auth/presentation/screens/sign_in_part_three_screen.dart
@@ -74,140 +74,156 @@ class _SignInPartThreeScreenState extends State<SignInPartThreeScreen> {
 
     return KeyboardDismisser(
       child: Scaffold(
-        body: Form(
-          key: _formKey,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20).r,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: SvgPicture.asset(
-                          Assets.tLeftArrowIcon,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      LinearPercentIndicator(
-                        barRadius: const Radius.circular(12).r,
-                        width: 120.w,
-                        lineHeight: 8.h,
-                        percent: 1,
-                        backgroundColor: const Color(0xff191B1C),
-                        progressColor: const Color(0xffEEEFF0),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(32).r,
-                            border: Border.all(color: const Color(0xffEEEFF0))),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                                  vertical: 8, horizontal: 16)
-                              .r,
-                          child: Text(
-                            "buttonHelp".tr(),
-                            style: TextStyle(
-                              fontFamily: "SfProDisplay",
-                              fontWeight: FontWeight.w500,
-                              fontSize: 12.sp,
-                              color: const Color(0xffEEEFF0),
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  child: IntrinsicHeight(
+                    child: SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20).r,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  icon: SvgPicture.asset(
+                                    Assets.tLeftArrowIcon,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                LinearPercentIndicator(
+                                  barRadius: const Radius.circular(12).r,
+                                  width: 120.w,
+                                  lineHeight: 8.h,
+                                  percent: 1,
+                                  backgroundColor: const Color(0xff191B1C),
+                                  progressColor: const Color(0xffEEEFF0),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(32).r,
+                                      border: Border.all(
+                                          color: const Color(0xffEEEFF0))),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                            vertical: 8, horizontal: 16)
+                                        .r,
+                                    child: Text(
+                                      "buttonHelp".tr(),
+                                      style: TextStyle(
+                                        fontFamily: "SfProDisplay",
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 12.sp,
+                                        color: const Color(0xffEEEFF0),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
                             ),
-                          ),
+                            SizedBox(
+                              height: 20.h,
+                            ),
+                            Text(
+                              "enterOTPcode".tr(),
+                              style: TextStyle(
+                                fontFamily: "SfProDisplay",
+                                fontWeight: FontWeight.w700,
+                                fontSize: 24.sp,
+                                color: const Color(0xffEEEFF0),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 8.h,
+                            ),
+                            Text(
+                              "signInPartThreeDescriptionOne".tr(),
+                              style: TextStyle(
+                                fontFamily: "SfProDisplay",
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16.sp,
+                                color: const Color(0xffB9BFC1),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 24.h,
+                            ),
+                            Center(
+                              child: Pinput(
+                                controller: _textEditingController,
+                                defaultPinTheme: defaultPinTheme,
+                                focusedPinTheme: focusedPinTheme,
+                                submittedPinTheme: submittedPinTheme,
+                                errorPinTheme: errorPinTheme,
+                                validator: (s) {
+                                  return s == '2222'
+                                      ? null
+                                      : 'Pin is incorrect';
+                                },
+                                pinputAutovalidateMode:
+                                    PinputAutovalidateMode.onSubmit,
+                                showCursor: false,
+                                onCompleted: (pin) => setState(
+                                  () {},
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 24.h,
+                            ),
+                            Text(
+                              "signInPartThreeDescriptionTwo".tr(),
+                              style: TextStyle(
+                                fontFamily: "SfProDisplay",
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16.sp,
+                                color: const Color(0xffB9BFC1),
+                              ),
+                            ),
+                            const Spacer(),
+                            Text(
+                              "signInPartThreeDescriptionThree".tr(),
+                              style: TextStyle(
+                                fontFamily: "SfProDisplay",
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16.sp,
+                                color: const Color(0xffB9BFC1),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 24.h,
+                            ),
+                            ButtonWidget(
+                              text: "continue".tr(),
+                              onTap: () {
+                                AnimatedNavigation.push(
+                                    context: context, page: const MainScreen());
+                              },
+                              isEnabled:
+                                  _formKey.currentState?.validate() ?? false,
+                            ),
+                            SizedBox(
+                              height: 45.h,
+                            ),
+                          ],
                         ),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  Text(
-                    "enterOTPcode".tr(),
-                    style: TextStyle(
-                      fontFamily: "SfProDisplay",
-                      fontWeight: FontWeight.w700,
-                      fontSize: 24.sp,
-                      color: const Color(0xffEEEFF0),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8.h,
-                  ),
-                  Text(
-                    "signInPartThreeDescriptionOne".tr(),
-                    style: TextStyle(
-                      fontFamily: "SfProDisplay",
-                      fontWeight: FontWeight.w400,
-                      fontSize: 16.sp,
-                      color: const Color(0xffB9BFC1),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 24.h,
-                  ),
-                  Center(
-                    child: Pinput(
-                      controller: _textEditingController,
-                      defaultPinTheme: defaultPinTheme,
-                      focusedPinTheme: focusedPinTheme,
-                      submittedPinTheme: submittedPinTheme,
-                      errorPinTheme: errorPinTheme,
-                      validator: (s) {
-                        return s == '2222' ? null : 'Pin is incorrect';
-                      },
-                      pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
-                      showCursor: false,
-                      onCompleted: (pin) => setState(
-                        () {},
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 24.h,
-                  ),
-                  Text(
-                    "signInPartThreeDescriptionTwo".tr(),
-                    style: TextStyle(
-                      fontFamily: "SfProDisplay",
-                      fontWeight: FontWeight.w400,
-                      fontSize: 16.sp,
-                      color: const Color(0xffB9BFC1),
-                    ),
-                  ),
-                  const Spacer(),
-                  Text(
-                    "signInPartThreeDescriptionThree".tr(),
-                    style: TextStyle(
-                      fontFamily: "SfProDisplay",
-                      fontWeight: FontWeight.w400,
-                      fontSize: 16.sp,
-                      color: const Color(0xffB9BFC1),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 24.h,
-                  ),
-                  ButtonWidget(
-                    text: "continue".tr(),
-                    onTap: () {
-                      AnimatedNavigation.push(
-                          context: context, page: const MainScreen());
-                    },
-                    isEnabled: _formKey.currentState?.validate() ?? false,
-                  ),
-                  SizedBox(
-                    height: 45.h,
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
+            );
+          },
         ),
       ),
     );
