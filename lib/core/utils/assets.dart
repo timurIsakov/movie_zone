ca64fa3669c66190d3655e1746f021f725477cdb
timurIsakov <timur092000@gmail.com>
2024-01-02 23:43:53 +0500

    Added "players_minimize_state_screen"

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -8,6 +8,8 @@ class Assets {
       "assets/images/brand_background_image.png";
   static const String tHelpBackground =
       "assets/images/help_background_image.png";
+  static const String tStrangerThingsImage =
+      "assets/images/stranger_things_image.png";
   //icon
   static const String tLeftArrowIcon = "assets/icons/left_arrow_icon.svg";
   static const String tPlayIcon = "assets/icons/play_icon.svg";
@@ -19,6 +21,13 @@ class Assets {
   static const String tSearchIcon = "assets/icons/search_icon.svg";
   static const String tPlayerIcon = "assets/icons/player_icon.svg";
   static const String tUserIcon = "assets/icons/user_icon.svg";
+  static const String tArrowBottomIcon = "assets/icons/arrow_bottom_icon.svg";
+  static const String tShareWifiIcon = "assets/icons/share_wifi_icon.svg";
+  static const String tBackward10Icon = "assets/icons/backward_10_icon.svg";
+  static const String tKebabMenuIcon = "assets/icons/kebab_menu_icon.svg";
+  static const String tForward10Icon = "assets/icons/forward_10_icon.svg";
+  static const String tPauseIcon = "assets/icons/pause_icon.svg";
+  static const String tReportIcon = "assets/icons/report_icon.svg";
   //svg
   static const String tNetflixLogo = "assets/logo/netflix_logo.svg";
 }
--- lib/features/main/presentation/widgets/movie_card_widget.dart
+++ lib/features/main/presentation/widgets/movie_card_widget.dart
@@ -6,69 +6,96 @@ import 'package:movie_zone/features/main/domain/entities/movie_entity.dart';
 
 class MovieCardWidget extends StatelessWidget {
   final MovieEntity movieEntity;
-  const MovieCardWidget({Key? key, required this.movieEntity})
+  final bool isHasPlayIcon;
+  const MovieCardWidget(
+      {Key? key, required this.movieEntity, this.isHasPlayIcon = false})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        SizedBox(
+        Container(
           height: 100.h,
           width: 140.w,
-          child: Image.asset(
-            movieEntity.post,
-            fit: BoxFit.cover,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(movieEntity.post), fit: BoxFit.cover)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (isHasPlayIcon)
+                Center(
+                  child: Container(
+                    height: 28.h,
+                    width: 28.w,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffEEEFF0),
+                      borderRadius: BorderRadius.circular(100).r,
+                    ),
+                    child: const Icon(
+                      Icons.play_arrow,
+                      size: 16,
+                    ),
+                  ),
+                ),
+            ],
           ),
         ),
         SizedBox(
           width: 8.w,
         ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              movieEntity.title,
-              style: TextStyle(
-                color: const Color(0xffEEEFF0),
-                fontFamily: "SfProDisplay",
-                fontWeight: FontWeight.w700,
-                fontSize: 18.sp,
-              ),
-            ),
-            SizedBox(
-              height: 12.h,
-            ),
-            Row(
-              children: [
-                Text(
-                  movieEntity.trailerDuration,
-                  style: TextStyle(
-                    color: const Color(0xffB9BFC1),
-                    fontFamily: "SfProDisplay",
-                    fontWeight: FontWeight.w500,
-                    fontSize: 14.sp,
-                  ),
-                ),
-                SizedBox(
-                  width: 12.w,
-                ),
-                Text(
-                  movieEntity.createdAt.year.toString(),
-                  style: TextStyle(
-                    color: const Color(0xffB9BFC1),
-                    fontFamily: "SfProDisplay",
-                    fontWeight: FontWeight.w500,
-                    fontSize: 14.sp,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
         SizedBox(
-          width: 21.w,
+          width: 140.h,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                movieEntity.title,
+                style: TextStyle(
+                  color: const Color(0xffEEEFF0),
+                  fontFamily: "SfProDisplay",
+                  fontWeight: FontWeight.w700,
+                  fontSize: 16.sp,
+                  height: 0.h,
+                ),
+              ),
+              SizedBox(
+                height: 12.h,
+              ),
+              Row(
+                children: [
+                  Text(
+                    movieEntity.trailerDuration,
+                    style: TextStyle(
+                      color: const Color(0xffB9BFC1),
+                      fontFamily: "SfProDisplay",
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.sp,
+                      height: 0.h,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 12.w,
+                  ),
+                  Text(
+                    movieEntity.createdAt.year.toString(),
+                    style: TextStyle(
+                      color: const Color(0xffB9BFC1),
+                      fontFamily: "SfProDisplay",
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.sp,
+                      height: 0.h,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
+        const Spacer(),
         SizedBox(
           height: 24.h,
           width: 24.w,
