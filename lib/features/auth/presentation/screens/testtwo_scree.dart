5d5381bd92c5ded096eeacee0a5528875cd29279
timurIsakov <timur092000@gmail.com>
2024-01-23 22:12:29 +0500

    added getMovies functionality

---
+++ lib/features/auth/presentation/screens/testtwo_scree.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TesttwoScree extends StatefulWidget {
+  const TesttwoScree({Key? key}) : super(key: key);
+  
+  @override
+  State<TesttwoScree> createState() => _TesttwoScreeState();
+  }
+
+  class _TesttwoScreeState extends State<TesttwoScree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
